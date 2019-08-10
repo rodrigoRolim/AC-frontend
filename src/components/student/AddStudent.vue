54b7856d1d560964e90cd95d5d19958f2f5e5036
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-08-10 20:23:59 -0300

    a new component to register a new student

--- src/components/student/AddStudent.vue
+++ src/components/student/AddStudent.vue
@@ -1,86 +1,89 @@
 <template>
-  <v-app>
+  <div>
     <ac-navbar>
-      <v-toolbar-items>
-        <v-btn flat to="/professor">professor</v-btn>
-        <v-btn flat to="/aluno">aluno</v-btn>
-      </v-toolbar-items>
+      <div class="list-nav">
+        <a to="/professor">professor</a>
+        <a to="/aluno">aluno</a>
+      </div>
     </ac-navbar>
-   
-    <v-layout justify-center class="container">
-      <v-flex xs12 sm10 md8 lg6>
-       <v-alert
-        :value="showAlert"
-        :type="alert"
-        >
-        {{ messageAlert }}
-       </v-alert>
-        <v-form ref="form">
-          <v-card >
-          
-            <v-card-text>
-              <v-text-field
-                ref="student.name"
-                v-model="student.name"
-                :rules="[() => !!student.name || 'Este campo é obrigatório']"
-                :error-messages="errorMessages"
-                label="Nome completo"
-                placeholder="John Doe"
-                required
-              ></v-text-field>
-              <v-text-field
-                ref="student.email"
-                v-model="student.email"
-                :error-messages="errorMessages"
-                :rules="[rules.required, rules.email]"
-                label="Email*"
-                placeholder="nome@email.com"
-                required
-              ></v-text-field>
-              <v-text-field
-                ref="student.ra"
-                v-model="student.ra"
-                :error-messages="errorMessages"
-                :rules="[() => !!student.ra || 'Este campo é obrigatório']"
-                label="Registro acadêmico (RA)*"
-                placeholder="a122345"
-                required
-              ></v-text-field>    
-              <v-autocomplete
-                ref="student.graduation"
-                v-model="student.graduation"
-                :error-messages="errorMessages"
-                :rules="[() => !!student.graduation || 'Este campo é obrigatório']"
-                :items="graduationsNames"
-                label="Graduação"
-                placeholder="Select..."
-                required
-              ></v-autocomplete>
-              <v-text-field
-                :append-icon="show ? 'visibility' : 'visibility_off'"
-                :rules="[rules.required, rules.min]"
-                :error-messages="errorMessages"
-                :type="show ? 'text' : 'password'"
-                name="input-10-2"
-                label="Crie uma senha"
-                hint="deve ter pelo menos 8 caracteres"
-                v-model="student.password"
-                class="input-group--focused"
-                @click:append="show = !show"
-              ></v-text-field>
-            </v-card-text>
-            <v-divider class="mt-5"></v-divider>
-            <v-card-actions class="justify-end">
-              <v-btn color="success" dark depressed to="/aluno">voltar</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn color="secondary" dark depressed @click="reset">Resetar</v-btn>
-              <v-btn color="primary"  depressed :disabled="validated" @click="createStudent">Submit</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-form>
-      </v-flex>
-    </v-layout>
-  </v-app>
+    <div class="container-add-student">
+      <div :class="validate.form.class" v-if="validate.form.show && validated">
+        <i class="far fa-times-circle"></i><p>{{validate.form.msg}}</p>
+      </div>
+      <form @submit.prevent="saveNewStudent()" ref="form" novalidate="true" 
+      v-checkform="{ fields: ['name', 'email', 'ra', 'graduation', 'password', 'confirm'], 
+      msg: 'preencha os campos abaixo', field: 'form' }">
+        <div class="name-div">
+          <label for="name">Nome completo*</label>
+          <input type="text" name="name" id="name"
+          v-model="student.name"
+          ref="name"
+          :class="validate.name.class"
+          v-required:keyup="{ field: 'name', msg: 'campo obrigatório', class: 'danger'}"
+         >
+         <small style="color: red" v-if="validate.name.show">{{validate.name.msg}}</small>
+        </div>
+        <div class="email-div">
+          <label for="email">Email institucional*</label> 
+          <input type="email" name="email" id="email"
+          v-model="student.email"
+          ref="email"
+          placeholder="digite o seu email da utfpr"
+          :class="validate.email.class"
+          v-email:keyup="{ field: 'email', msg: 'email inválido', class: 'danger' }"
+          >
+           <small style="color: red" v-if="validate.email.show">{{validate.email.msg}}</small>
+        </div>
+        <div class="ra-div">
+          <label for="ra">Registro Acadêmico (RA)*</label>
+          <input type="text" name="ra" id="ra"  placeholder="ex: a123457"
+          v-model="student.ra"
+          ref="ra"
+          :class="validate.ra.class"
+          v-required:keyup="{ field: 'ra', msg: 'campo obrigatório', class: 'danger' }"
+          >
+          <small style="color: red" v-if="validate.ra.show">{{validate.ra.msg}}</small>
+        </div>
+        <div class="graduation-div">
+          <label for="graduation">Curso de graduação*</label>
+          <select name="graduation" id="graduation" placeholder="selecion o seu curso"
+          v-model="student.graduation"
+          ref="graduation"
+          :class="validate.graduation.class"
+          v-required:change="{ field: 'graduation', msg: 'campo obrigatório', class: 'danger' }"
+          >
+            <option value disabled selected hidden></option>
+            <option value="aasd">option 2</option>
+          </select>
+          <small style="color: red" v-if="validate.graduation.show">{{validate.graduation.msg}}</small>
+        </div>
+        <div class="password-div">
+          <label for="password">Crie uma senha*</label>
+          <input type="password" name="password" id="password"
+          v-model="student.password" 
+          ref="password"
+          placeholder="crie um senha"
+          :class="validate.password.class"
+          v-min:keyup="{ field: 'password', msg: 'mínimo de 8 caractéres', tam: 8, class: 'danger' }"
+          >
+          <small style="color: red" v-if="validate.password.show">{{validate.password.msg}}</small>
+        </div>
+        <div class="confirm-password-div">
+          <label for="confirm-password">Confirme a senha*</label>
+          <input type="password" name="confirm-password" id="confirm-password"
+          v-model="confirm"
+          ref="confirm"
+          :class="validate.confirm.class"
+          v-equal:keyup="{ field: 'confirm', msg: 'as senhas não combinam', equalto: 'password', class: 'danger'}"
+          >
+          <small style="color: red" v-if="validate.confirm.show">{{validate.confirm.msg}}</small>
+        </div>
+        <div class="control-btns">
+          <button type="submit" >cadastrar</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -88,37 +91,38 @@ import AcNavbar from '../AcNavbar'
 import router from '@/router/index'
 import Student from '@/services/Student.js'
 import GraduationService from '@/services/Graduation.js'
+import { setTimeout } from 'timers';
+import message from '../../mixins/messages'
 
 export default {
   name: 'AddStudent',
   components: { AcNavbar },
+  mixins: [message],
   data () {
     return {
-      show: false,
-      errorMessages: '',
-      alert: null,
-      showAlert: false,
-      messageAlert: '',
+      validate: {
+        name: { msg: 'campo obrigatório', class: '', show: false },
+        ra: { msg: 'campo obrigatório', class: '', show: false },
+        email: { msg: '', class: '', show: false },
+        graduation: { msg: 'campo obrigatório', class: '', show: false },
+        password: { msg: '', class: '', show: false },
+        confirm: { msg: '', class: '', show: false },
+        form: { msg: '', class: 'required-fields', show: false}
+      },
       student: {
         ra: null,
         name: null,
         email: null,
         graduation: null,
-        password: ''
+        password: null
       },
-      rules: {
-        required: value => !!value || 'Campo obrigatório',
-        email: value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Email inválido.'
-        },
-        min: value => typeof value == 'undefined' || value.length >= 8 || 'Min de 8 caracteres'
-      },
-      graduationsNames: [],
-      graduations: []
+      confirm: null,
+      pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     }
   },
   created () {
+  // console.log(this.getConfirmation('error', 'asdasd'))
+/* 
     GraduationService.readAll()
       .then((resp) => {
         this.graduations = resp.data
@@ -126,21 +130,31 @@ export default {
           this.graduationsNames.push(graduation.name)
         })
       })
-      .catch((err) => this.getConfirmation('error', 'Ocorreu um erro, tente mais uma vez'))
+      .catch((err) => this.getConfirmation('error', 'Ocorreu um erro, tente mais uma vez')) */
   },
   computed: {
-    form () {
-      return this.student
-    },
+
     validated () {
       return this.student.name == null ||  
                this.student.email == null || 
+               !this.pattern.test(this.student.email) || 
                this.student.ra == null ||
                this.student.graduation == null ||
-               this.student.password == '' 
-    }
+               this.student.password == null || 
+               this.student.password.length <= 0 ||
+               this.confirm == null || 
+               this.confirm !== this.student.password
+    } 
   },
   methods: {
+    saveNewStudent () {
+ 
+      if (!this.validated) {
+        console.log("achiviement request")
+        return
+      }
+      this.validated
+    },
     createStudent () {
       this.student.department = this.catchIdDepartment()
       Student.create(this.student).then(response => {
@@ -160,7 +174,6 @@ export default {
         this.showAlert = false
         this.alert = null
       }, 3000)
-      this.reset()
     },
     catchIdDepartment () {
       const graduation = this.graduations.filter(grad => 
@@ -168,15 +181,135 @@ export default {
       const id = graduation[0].deps[0]._id
       return id
     },
-    reset () {
-     this.$refs.form.reset()
-    }
   }
 }
 </script>
 
 <style scoped>
-.container {
-  padding: calc(18vh/2) 0;
+form {
+  display: flex;
 }
+.container-add-student {
+  display: flex;
+  flex-direction: column;
+  padding: calc(18vh/2) 0;
+  align-items: center;
+  width: 100%;
+}
+input {
+  padding: 10px 0;
+  text-indent: 8px;
+}
+input, select {
+  border-radius: 3px;
+  border: 1px solid #d1d5da;
+}
+option[disabled] {
+  color: #d1d5da
+}
+select {
+  background-color: white;
+  padding: 9px 0;
+  text-indent: 6px;
+}
+.control-btns {
+  display: flex;
+  flex-direction: row;
+}
+button[type="submit"] {
+  background-color: rgba(73, 161, 73, 0.8);
+  border: 1px solid transparent;
+  border-radius: 1px;
+  margin-top: 50px;
+  color: white;
+  font-size: 1.0rem;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+  box-shadow:  inset 1px 2px 6px rgba(128, 125, 125, 0.288);
+}
+button[type="submit"]:active {
+  background-color: rgba(105, 230, 105, 0.808);
+}
+button[type="submit"]:disabled {
+  background-color: rgba(146, 146, 146, 0.527);
+}
+.danger {
+  border: 1px solid red;
+}
+.ok {
+  border: 1px solid #d1d5da;
+}
+.required-fields {
+  border: 1px solid rgb(250, 142, 142);
+  color: white;
+  background-color: red;
+  border-radius: 2px;
+  font-size: 1.0rem;
+  flex-direction: row;
+  align-items: center;
+  font-weight: 600;
+  margin-bottom: 30px;
+  justify-content: center;
+}
+i {
+  margin-right: 20px;
+  font-size: 1.5rem;
+}
+::-webkit-input-placeholder {text-indent:8px!important; font-size: 1.0rem}
+:-moz-placeholder { text-indent:8px!important; font-size: 1.0rem}
+::-moz-placeholder {text-indent:8px!important; font-size: 1.0rem}
+::-ms-input-placeholder {text-indent:8px!important; font-size: 1.0rem} 
+@media only screen and (max-width: 360px) {
+  .required-fields {
+    display: flex;
+    padding: 20px 0;
+    width: 90%;
+  }
+  form {
+    flex-direction: column;
+    width: 90%;
+  }
+  [class*="-div"] input, select {
+    margin-top: 3px;
+    width: 100%;
+  }
+  [class*="-div"] {
+    margin-top: 10px;
+  }
+  .control-btns {
+    flex-direction: column;
+  }
+  button[type="submit"] {
+    padding: 15px 0;
+  }
+}
+@media only screen and (min-width: 360px) and (max-width: 500px) {
+  .required-fields {
+    width: 90%
+  }
+  form {
+    flex-direction: column;
+    width: 90%;
+  }
+  [class*="-div"] input, select {
+    margin-top: 3px;
+    width: 100%;
+  }
+  [class*="-div"] {
+    margin-top: 10px;
+  }
+  input, select {
+    padding: 13px 0;
+    font-size: 1.05rem;
+  }
+  button[type="submit"] {
+    padding: 18px 0;
+    font-size: 1.1rem;
+  }
+  .control-btns {
+    flex-direction: column;
+  }
+}
+
 </style>
