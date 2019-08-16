6f3ca8de642ab1e36826c727b6fd401ef2ceed44
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-08-15 23:50:46 -0300

    add indent into inputs

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,11 @@ export default {
   font-size: 0.9rem;
   color:rgb(53, 52, 52)
 }
+
+::-webkit-input-placeholder {text-indent:8px!important; font-size: 1.0rem}
+:-moz-placeholder { text-indent:8px!important; font-size: 1.0rem}
+::-moz-placeholder {text-indent:8px!important; font-size: 1.0rem}
+::-ms-input-placeholder {text-indent:8px!important; font-size: 1.0rem} 
 @media only screen and (min-width: 361px) and (max-width: 500px) {
   #app {
     font-size: 1.05rem;
